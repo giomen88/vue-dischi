f0b9798cda9fd5a156f2dcddc441f8176385f3ee
giomen88 <giorgia9288@virgilio.it>
2022-07-26 18:27:18 +0200

    esercizio ok (no bonus)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
+
+    <TheHeader />
+
+    <TheMain />
+
   </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue'
+import TheMain from './components/TheMain.vue'
 
 export default {
   name: 'App',
   components: {
+    TheHeader,
+    TheMain
   }
 }
+
+
 </script>
 
 <style lang="scss">
-
+@import './assets/sass/style.scss'
 </style>
---
+++ src/components/TheMain.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <DiscCard />
+    </main>
+</template>
+
+<script>
+import DiscCard from './DiscCard.vue'
+export default {
+    name: 'TheMain',
+    components: {
+        DiscCard,
+    },
+    data() { return {
+    }},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
